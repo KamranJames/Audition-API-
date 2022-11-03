b29c7200ba8616a89ea778663d37cb84237fe830
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2022-11-03 18:02:52 +1100

    EOD commit

--- main.py
+++ main.py
@@ -39,10 +39,12 @@ def create_app():
       
    
    ##Config Section for app
-   app.config ['JSON_SORT_KEYS'] = False
+
+   app.config['JSON_SORT_KEYS'] = False
    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
-    
+   
+
    db.init_app(app)
    ma.init_app(app)
    bcrypt.init_app(app)
